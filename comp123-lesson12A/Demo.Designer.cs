856c3316a6263cbae04b0352f39cc24416389589
mehul <mkhosla2@my.centennialcollege.ca>
2017-08-01 16:27:28 -0400

    added button and text box

--- comp123-lesson12A/Demo.Designer.cs
+++ comp123-lesson12A/Demo.Designer.cs
@@ -28,20 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.HelloLable = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // HelloButton
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 357);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.HelloButton.Location = new System.Drawing.Point(93, 46);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(100, 38);
+            this.HelloButton.TabIndex = 0;
+            this.HelloButton.Text = "Say Hello";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // HelloLable
+            // 
+            this.HelloLable.AutoSize = true;
+            this.HelloLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelloLable.Location = new System.Drawing.Point(51, 9);
+            this.HelloLable.Name = "HelloLable";
+            this.HelloLable.Size = new System.Drawing.Size(202, 25);
+            this.HelloLable.TabIndex = 1;
+            this.HelloLable.Text = "CLICK ON BUTTON";
+            this.HelloLable.Click += new System.EventHandler(this.HelloLable_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.HelloButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.InputTextBox, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 129);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 87);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(83, 10);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(110, 20);
+            this.InputTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ENTER NAME";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Demo
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HelloLable);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Demo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DEMO FORM";
+            this.Load += new System.EventHandler(this.Demo_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button HelloButton;
+        private System.Windows.Forms.Label HelloLable;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- comp123-lesson12A/Demo.cs
+++ comp123-lesson12A/Demo.cs
@@ -20,5 +20,36 @@ namespace comp123_lesson12A
         {
             InitializeComponent();
         }
+
+        private void Demo_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// this is the event handler for helloButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (InputTextBox.Text.Length > 0)
+            {
+                HelloLable.Text = "Hello, World!!" + InputTextBox.Text;
+
+            }
+            else
+            {
+                HelloLable.Text = "hello world!!";
+            }
+        }
+        private void HelloLable_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
